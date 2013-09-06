e885701a12fcb2d2557c975fadbabc7ee28ebf8b
Peter Downs <pdowns@godaddy.com>
2013-09-06 20:39:02 +0000

    Add helper for updating URL GET parameters.

--- djoauth2/helpers.py
+++ djoauth2/helpers.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 import random
+import urlparse
 from string import ascii_letters, digits
-
+from urllib2 import urlencode
 
 # From http://tools.ietf.org/html/rfc6750#section-2.1
 BEARER_TOKEN_CHARSET = ascii_letters + digits + '-._~+/'
@@ -10,6 +11,21 @@ BEARER_TOKEN_CHARSET = ascii_letters + digits + '-._~+/'
 def random_hash(length):
   return ''.join(random.sample(BEARER_TOKEN_CHARSET, length))
 
+
 def random_hash_generator(length):
   return lambda: random_hash(length)
+
+
+def update_parameters(url, parameters):
+  """ Updates a URL's existing GET parameters.
+
+  @url: a URL string.
+  @parameters: a dictionary of parameters, {string:string}.
+  """
+  parsed_url = urlparse(url)
+
+  query_parameters = urlparse.parse_qsl(parsed_url.query)
+  parsed_url.query = urlencode(query_parameters + parameters.items())
+
+  return urlparse.urlunparse(parsed_url)
 
