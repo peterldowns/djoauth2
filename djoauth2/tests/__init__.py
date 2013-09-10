16c1ecd66df8b6160b73173284967a38c4a68b75
Peter Downs <peterldowns@gmail.com>
2013-09-09 21:32:13 -0700

    Add tests for AccessToken endpoint.

--- djoauth2/tests/__init__.py
+++ djoauth2/tests/__init__.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import datetime
 import json
 from hashlib import md5
 from random import random
@@ -25,7 +26,6 @@ class DJOAuth2TestClient(TestClient):
     self.authorization_endpoint = '/oauth2/authorization/'
     self.token_endpoint = '/oauth2/token/'
     self.scope_names = scope_names or []
-    self.ssl_only = settings.DJOAUTH2_SSL_ONLY
 
     # For internal use
     self.history = []
@@ -33,6 +33,10 @@ class DJOAuth2TestClient(TestClient):
     self.refresh_token = None
     self.lifetime = None
     super(DJOAuth2TestClient, self).__init__()
+
+  @property
+  def ssl_only(self):
+    return settings.DJOAUTH2_SSL_ONLY
 
   @property
   def scope_string(self):
@@ -66,7 +70,8 @@ class DJOAuth2TestClient(TestClient):
     if use_ssl is None:
       use_ssl = self.ssl_only
 
-    response = self.post(self.token_endpoint, data=data, **{
+    request_method = getattr(self, method.lower())
+    response = request_method(self.token_endpoint, data=data, **{
       'wsgi.url_scheme': 'https' if use_ssl else 'http'})
     self.load_token_data(response)
     return response
@@ -217,22 +222,20 @@ class DJOAuth2TestCase(TestCase):
     # Check the response contents
     self.assertIsNone(self.oauth_client.access_token)
     self.assertIsNone(self.oauth_client.refresh_token)
-    self.assertIsNone(self.oauth_client.expires_in)
+    self.assertIsNone(self.oauth_client.lifetime)
 
 
 class TestAccessToken(DJOAuth2TestCase):
   def test_pass_no_redirect_defaults_to_registered(self):
-    """ If the OAuth client has registered a redirect uri, it is OK to
-    not explicitly pass the same URI again.
+    """ If the OAuth client has registered a redirect uri, it is OK to not
+    explicitly pass the same URI again.
     """
-    # Create a client with a specific redirect URI.
     self.initialize()
 
     # Create an authorization code without a redirect URI.
-    authcode = self.create_authorization_code(
-        self.user,
-        self.client,
-        {'redirect_uri' : None})
+    authcode = self.create_authorization_code(self.user, self.client, {
+        'redirect_uri' : None
+      })
 
     # Override the default redirect param to not exist.
     response = self.oauth_client.request_access_token(
@@ -243,4 +246,195 @@ class TestAccessToken(DJOAuth2TestCase):
         })
 
     self.assert_token_success(response)
+
+  def test_passed_uri_must_match_registered(self):
+    """ If the OAuth client has registered a redirect uri, and the same
+    redirect URI is passed here, the request should succeed.
+    """
+    self.initialize()
+
+    # Create an authorization code, which must have a redirect because there is
+    # no default redirect for this client
+    authcode = self.create_authorization_code(self.user, self.client, {
+          'redirect_uri' : self.client.redirect_uri
+        })
+
+    # Request an authorization token with the same redirect as the
+    # authorization code (the OAuth spec requires them to match.)
+    response = self.oauth_client.request_access_token(
+        self.client,
+        authcode.value,
+        custom={
+          'redirect_uri' : self.client.redirect_uri,
+        })
+
+    self.assert_token_success(response)
+
+  def test_redirect_uri_does_not_match_registered_uri(self):
+    """ If the OAuth client has registered a redirect uri, and passes a
+    different redirect URI to the access token request, the request will fail.
+    """
+    self.initialize()
+
+    # Request an authorization token with a redirect that is different than the
+    # one registered by the client.
+
+    authcode = self.create_authorization_code(self.user, self.client, {
+          'redirect_uri' : self.client.redirect_uri
+        })
+
+    different_redirect = 'https://NOTlocu.com'
+    self.assertNotEqual(different_redirect, self.client.redirect_uri)
+
+    response = self.oauth_client.request_access_token(
+        self.client,
+        authcode.value,
+        custom={
+          'redirect_uri' : different_redirect,
+        })
+
+    self.assert_token_failure(response)
+
+  def test_secure_request_succeeds_when_ssl_not_required(self):
+    """ If the OAuth client has registered a secure redirect uri, and SSL is
+    not required by the server, then requests will still succeed.
+    """
+    self.initialize()
+    settings.DJOAUTH2_SSL_ONLY = False
+
+    authcode = self.create_authorization_code(self.user, self.client)
+
+    response = self.oauth_client.request_access_token(
+        self.client, authcode.value, use_ssl=True)
+
+    self.assert_token_success(response)
+
+  def test_insecure_request_succeeds_when_ssl_not_required(self):
+    """ If the OAuth client has registered an insecure redirect uri, and
+    SSL is not required by the server, then requests will succeed.
+    """
+    self.initialize()
+    settings.DJOAUTH2_SSL_ONLY = False
+
+    authcode = self.create_authorization_code(self.user, self.client)
+
+    response = self.oauth_client.request_access_token(
+        self.client, authcode.value, use_ssl=False)
+
+    self.assert_token_success(response)
+
+  def test_insecure_request_fails_when_ssl_required(self):
+    self.initialize()
+    settings.DJOAUTH2_SSL_ONLY = True
+
+    authcode = self.create_authorization_code(self.user, self.client)
+
+    response = self.oauth_client.request_access_token(
+        self.client, authcode.value, use_ssl=False)
+
+    self.assert_token_failure(response)
+
+  def test_missing_secret(self):
+    """ If the access token request does not include a secret, it will fail. """
+    self.initialize()
+
+    authcode = self.create_authorization_code(self.user, self.client)
+
+    # Override default client_secret param to not exist.
+    response = self.oauth_client.request_access_token(
+        self.client,
+        authcode.value,
+        custom={
+          'client_secret' : None
+        })
+
+    self.assert_token_failure(response)
+
+  def test_mismatched_secret(self):
+    """ If the access token request includes a secret that doesn't match the
+    registered secret, the request will fail.
+    """
+    self.initialize()
+
+    authcode = self.create_authorization_code(self.user, self.client)
+
+    # Override default client_secret param to not match the client's registered
+    # secret.
+    mismatched_secret = self.client.secret + 'thischangesthevalue'
+    self.assertNotEqual(mismatched_secret, self.client.secret)
+
+    response = self.oauth_client.request_access_token(
+        self.client,
+        authcode.value,
+        custom={
+          'client_secret' : mismatched_secret
+        })
+
+    self.assert_token_failure(response)
+
+  def test_mismatched_code_and_client(self):
+    """ If the code authorized by a user is not associated with the OAuth
+    client making the access token request, the request will fail.
+    """
+    self.initialize()
+
+    default_client_authcode = self.create_authorization_code(
+        self.user, self.client)
+
+    # Prove that the second OAuth client does not have the same key or secret
+    # as the default OAuth client.
+    self.assertNotEqual(default_client_authcode.client.key, self.client2.key)
+    self.assertNotEqual(default_client_authcode.client.secret,
+                        self.client2.secret)
+
+    response = self.oauth_client.request_access_token(
+        self.client2, default_client_authcode.value)
+
+    self.assert_token_failure(response)
+
+  def test_expired_code(self):
+    """ If an authorization code is unused within its lifetime, an attempt to
+    use it will fail.
+    """
+    self.initialize()
+
+    # Modify the authcode's date_created timestamp to be sufficiently far in
+    # the past that it is now expired.
+    authcode = self.create_authorization_code(self.user, self.client)
+    authcode.date_created -= datetime.timedelta(seconds=authcode.lifetime)
+    authcode.save()
+    self.assertTrue(authcode.is_expired())
+
+    response = self.oauth_client.request_access_token(
+        self.client, authcode.value)
+
+    self.assert_token_failure(response)
+
+  def test_invalid_grant(self):
+    """ If an Authorization Code / Grant does not exist, then the request will
+    fail.
+    """
+    self.initialize()
+
+    authcode = self.create_authorization_code(self.user, self.client)
+    fake_authcode_value = "myfakeauthcodelol"
+    self.assertNotEqual(authcode, fake_authcode_value)
+    self.assertFalse(
+        AuthorizationCode.objects.filter(value=fake_authcode_value).exists())
+
+    response = self.oauth_client.request_access_token(
+        self.client, fake_authcode_value)
+
+    self.assert_token_failure(response)
+
+  def test_get_requests_fail(self):
+    """ The Access Token endpoint should not accept GET requests -- only POST.
+    """
+    self.initialize()
+
+    authcode = self.create_authorization_code(self.user, self.client)
+    response = self.oauth_client.request_access_token(
+        self.client, authcode.value, method='GET')
+
+    self.assert_token_failure(response)
 
