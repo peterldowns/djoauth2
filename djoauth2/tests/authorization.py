618a510ab48e52e01d9d9c49b6b13abc78185d80
Peter Downs <peterldowns@gmail.com>
2013-09-17 16:24:46 -0700

    Add even more Authorization tests.

--- djoauth2/tests/authorization.py
+++ djoauth2/tests/authorization.py
@@ -2,6 +2,7 @@
 import sys
 from urlparse import urlparse
 from urlparse import parse_qsl
+from urlparse import urlunparse
 
 from django.conf import settings
 from django.conf.urls.defaults import patterns
@@ -14,6 +15,7 @@ from djoauth2.authorization import InvalidRequest
 from djoauth2.authorization import InvalidScope
 from djoauth2.authorization import UnauthenticatedUser
 from djoauth2.authorization import UnsupportedResponseType
+from djoauth2.helpers import update_parameters
 from djoauth2.models import Scope
 from djoauth2.models import Client
 from djoauth2.tests import test_urls
@@ -77,7 +79,6 @@ class TestAuthorizationCodeEndpoint(DJOAuth2TestCase):
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.content, 'OK')
 
-
   def test_post_requests_succeed(self):
     """ [The authorization server] MAY suport the use of the HTTP "POST" method
     [for the authorization endpoint] as well.
@@ -100,8 +101,6 @@ class TestAuthorizationCodeEndpoint(DJOAuth2TestCase):
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.content, 'OK')
 
-
-  ## SSL
   def test_ssl_required_secure_request_succeeds(self):
     """ When SSL is required (as per spec), secure requests should succeed. """
     settings.DJOAUTH2_SSL_ONLY = True
@@ -178,7 +177,6 @@ class TestAuthorizationCodeEndpoint(DJOAuth2TestCase):
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.content, 'OK')
 
-  # Authentication
   def test_user_not_authenticated_fails(self):
     """ The Authorization endpoint requires a logged-in user to accept or deny
     the request. If the user is not logged in, request can not be granted, and
@@ -218,7 +216,6 @@ class TestAuthorizationCodeEndpoint(DJOAuth2TestCase):
           },
           endpoint=self.dummy_endpoint_uri)
 
-  # State
   def test_state_required_and_no_state_included_fails(self):
     """ When the "state" parameter is required (as recommended by the spec),
     requests that omit the parameter should fail.
@@ -377,7 +374,6 @@ class TestAuthorizationCodeEndpoint(DJOAuth2TestCase):
     self.assertIn('state', parsed_url_parameters)
     self.assertEqual(parsed_url_parameters['state'], state_value)
 
-  ## Scope
   def test_no_scope_included_fails(self):
     """ Authorization requests that do not include a "scope" parameter should
     fail.
@@ -444,7 +440,6 @@ class TestAuthorizationCodeEndpoint(DJOAuth2TestCase):
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.content, 'OK')
 
-
   def test_multiple_scopes_included_succeeds(self):
     """ Authorization requests that request access to multiple Scopes should
     succeed.
@@ -467,7 +462,6 @@ class TestAuthorizationCodeEndpoint(DJOAuth2TestCase):
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.content, 'OK')
 
-  # Client ID
   def test_no_client_id_included_fails(self):
     """ Authorization requests must include a "client_id" parameter; if they
     do not, they should fail.
@@ -512,28 +506,266 @@ class TestAuthorizationCodeEndpoint(DJOAuth2TestCase):
           },
           endpoint=self.dummy_endpoint_uri)
 
-  ## Redirect URI
-  #def test_included_redirect_matches_registered_succeeds(self):
-  #  raise NotImplementedError()
-  #def test_included_redirect_does_not_match_registered_fails(self):
-  #  raise NotImplementedError()
-  #def test_non_absolute_redirect_uri_fails(self):
-  #  raise NotImplementedError()
-  #def test_redirect_uri_query_parameters_preserved_on_success(self):
-  #  raise NotImplementedError()
-  #def test_redirect_uri_query_parameters_preserved_on_error(self):
-  #  raise NotImplementedError()
+  def test_included_redirect_matches_registered_succeeds(self):
+    """ If the request includes a "redirect_uri" parameter, it must match the
+    registered value. If it does match, the request should succeed.
+    """
+    self.initialize(scope_names=['verify'])
 
-  ## User interaction
-  #def test_error_response_redirects_to_valid_uri(self):
-  #  raise NotImplementedError()
-  #def test_error_response_does_not_redirect_to_non_absolute_uri(self):
-  #  raise NotImplementedError()
-  #def test_ssl_required_error_response_does_not_redirect_to_non_secure_uri(self):
-  #  raise NotImplementedError()
-  #def test_no_ssl_required_error_response_does_redirect_to_non_secure_uri(self):
-  #  raise NotImplementedError()
+    auth_code_generator = AuthorizationCodeGenerator(self.missing_redirect_uri)
 
+    make_validation_endpoint(self.dummy_endpoint_uri, auth_code_generator)
+
+    self.oauth_client.login(username=self.user.username, password='password')
+
+    response = self.oauth_client.make_authorization_request(
+        client_id=self.client.key,
+        scope_string=self.oauth_client.scope_string,
+        custom={
+          'redirect_uri': self.client.redirect_uri,
+        },
+        endpoint=self.dummy_endpoint_uri)
+
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.content, 'OK')
+
+  def test_included_redirect_does_not_match_registered_fails(self):
+    """ If the request includes a "redirect_uri" parameter, it must match the
+    registered value. If it does not match, the request should fail.
+    """
+    self.initialize(scope_names=['verify'])
+
+    auth_code_generator = AuthorizationCodeGenerator(self.missing_redirect_uri)
+
+    make_validation_endpoint(self.dummy_endpoint_uri, auth_code_generator)
+
+    self.oauth_client.login(username=self.user.username, password='password')
+
+    different_redirect_uri = 'https://peterdowns.com'
+    self.assertNotEqual(different_redirect_uri, self.client.redirect_uri)
+
+    with self.assertRaises(InvalidRequest):
+      response = self.oauth_client.make_authorization_request(
+          client_id=self.client.key,
+          scope_string=self.oauth_client.scope_string,
+          custom={
+            'redirect_uri': different_redirect_uri,
+          },
+          endpoint=self.dummy_endpoint_uri)
+
+  def test_non_absolute_redirect_uri_fails(self):
+    """ The redirection endpoint URI MUST be an absolute URI.
+
+    See http://tools.ietf.org/html/rfc6749#section-3.1.2 .
+    """
+    self.initialize(scope_names=['verify'])
+
+    auth_code_generator = AuthorizationCodeGenerator(self.missing_redirect_uri)
+
+    make_validation_endpoint(self.dummy_endpoint_uri, auth_code_generator)
+
+    self.oauth_client.login(username=self.user.username, password='password')
+
+    self.client.redirect_uri = '/relative/url/'
+    self.client.save()
+
+    with self.assertRaises(InvalidRequest):
+      response = self.oauth_client.make_authorization_request(
+          client_id=self.client.key,
+          scope_string=self.oauth_client.scope_string,
+          endpoint=self.dummy_endpoint_uri)
+
+  def test_redirect_uri_query_parameters_preserved_on_success(self):
+    """ Redirection endpoint URIs MAY include a query component, which MUST be
+    retained when adding additional query parameters, such as during the
+    success redirect.
+    """
+    self.initialize(scope_names=['verify'])
+
+    auth_code_generator = AuthorizationCodeGenerator(self.missing_redirect_uri)
+
+    make_validation_endpoint(self.dummy_endpoint_uri, auth_code_generator)
+
+    self.oauth_client.login(username=self.user.username, password='password')
+
+    parameters_dict = {
+        'value1': 'True',
+        'value2': '4242424242',
+        'foo': 'bar',
+      }
+    self.client.redirect_uri = update_parameters('https://locu.com/',
+                                                 parameters_dict)
+    self.client.save()
+
+    response = self.oauth_client.make_authorization_request(
+        client_id=self.client.key,
+        scope_string=self.oauth_client.scope_string,
+        endpoint=self.dummy_endpoint_uri)
+
+    successful_redirect = auth_code_generator.make_success_redirect()
+
+    # Grab the URL parameters from the redirect response's Location header and
+    # turn them into a dict object.
+    parsed_url_parameters = dict(
+        parse_qsl(urlparse(successful_redirect.get('location')).query))
+
+    for parameter, value in parameters_dict.iteritems():
+      self.assertIn(parameter, parsed_url_parameters)
+      self.assertEqual(parsed_url_parameters[parameter], value)
+
+  def test_redirect_uri_query_parameters_preserved_on_error(self):
+    """ Redirection endpoint URIs MAY include a query component, which MUST be
+    retained when adding additional query parameters, such as during the
+    error redirect.
+    """
+    self.initialize(scope_names=['verify'])
+
+    auth_code_generator = AuthorizationCodeGenerator(self.missing_redirect_uri)
+
+    make_validation_endpoint(self.dummy_endpoint_uri, auth_code_generator)
+
+    self.oauth_client.login(username=self.user.username, password='password')
+
+    parameters_dict = {
+        'value1': 'True',
+        'value2': '4242424242',
+        'foo': 'bar',
+      }
+    self.client.redirect_uri = update_parameters('https://locu.com/',
+                                                 parameters_dict)
+    self.client.save()
+
+    response = self.oauth_client.make_authorization_request(
+        client_id=self.client.key,
+        scope_string=self.oauth_client.scope_string,
+        endpoint=self.dummy_endpoint_uri)
+
+    successful_redirect = auth_code_generator.make_error_redirect()
+
+    # Grab the URL parameters from the redirect response's Location header and
+    # turn them into a dict object.
+    parsed_url_parameters = dict(
+        parse_qsl(urlparse(successful_redirect.get('location')).query))
+
+    for parameter, value in parameters_dict.iteritems():
+      self.assertIn(parameter, parsed_url_parameters)
+      self.assertEqual(parsed_url_parameters[parameter], value)
+
+  def test_error_response_redirects_to_valid_uri(self):
+    """ When a registered redirect URI is valid, the error redirect should
+    direct the user to the registered URI.
+    """
+    self.initialize(scope_names=['verify'])
+
+    auth_code_generator = AuthorizationCodeGenerator(self.missing_redirect_uri)
+
+    make_validation_endpoint(self.dummy_endpoint_uri, auth_code_generator)
+
+    self.oauth_client.login(username=self.user.username, password='password')
+
+    # This redirect_uri is absolute and secure -- therefore, valid.
+    self.client.redirect_uri = 'https://locu.com/'
+    self.client.save()
+
+    response = self.oauth_client.make_authorization_request(
+        client_id=self.client.key,
+        scope_string=self.oauth_client.scope_string,
+        endpoint=self.dummy_endpoint_uri)
+
+    # Even though the request was successful, create an error redirect.
+    error_redirect = auth_code_generator.make_error_redirect()
+
+    # Show that the error redirect was directed to the valid, registered URI
+    # and not the default URI to use when the registered URI is invalid or
+    # missing. Do not check query parameters (subject of another test.)
+    parsed_redirect_uri = urlparse(error_redirect.get('location'))
+    redirect_location = urlunparse((
+      parsed_redirect_uri.scheme,
+      parsed_redirect_uri.netloc,
+      parsed_redirect_uri.path,
+      '',
+      '',
+      ''))
+
+    self.assertEqual(redirect_location, self.client.redirect_uri)
+    self.assertNotEqual(redirect_location, self.missing_redirect_uri)
+
+
+  def test_success_response_redirects_to_valid_uri(self):
+    """ When a registered redirect URI is valid, the success redirect should
+    direct the user to the registered URI.
+    """
+    self.initialize(scope_names=['verify'])
+
+    auth_code_generator = AuthorizationCodeGenerator(self.missing_redirect_uri)
+
+    make_validation_endpoint(self.dummy_endpoint_uri, auth_code_generator)
+
+    self.oauth_client.login(username=self.user.username, password='password')
+
+    # This redirect_uri is absolute and secure -- therefore, valid.
+    self.client.redirect_uri = 'https://locu.com/'
+    self.client.save()
+
+    response = self.oauth_client.make_authorization_request(
+        client_id=self.client.key,
+        scope_string=self.oauth_client.scope_string,
+        endpoint=self.dummy_endpoint_uri)
+
+    success_redirect = auth_code_generator.make_success_redirect()
+
+    # Show that the error redirect was directed to the valid, registered URI
+    # and not the default URI to use when the registered URI is invalid or
+    # missing. Do not check query parameters (subject of another test.)
+    parsed_redirect_uri = urlparse(success_redirect.get('location'))
+    redirect_location = urlunparse((
+      parsed_redirect_uri.scheme,
+      parsed_redirect_uri.netloc,
+      parsed_redirect_uri.path,
+      '',
+      '',
+      ''))
+
+    self.assertEqual(redirect_location, self.client.redirect_uri)
+    self.assertNotEqual(redirect_location, self.missing_redirect_uri)
+
+  def test_error_response_does_not_redirect_to_non_absolute_uri(self):
+    """ When a registered redirect URI is valid, the success redirect should
+    direct the user to the registered URI.
+    """
+    self.initialize(scope_names=['verify'])
+
+    auth_code_generator = AuthorizationCodeGenerator(self.missing_redirect_uri)
+
+    make_validation_endpoint(self.dummy_endpoint_uri, auth_code_generator)
+
+    self.oauth_client.login(username=self.user.username, password='password')
+
+    # This redirect_uri is now NOT absolute, in violation of the spec.
+    self.client.redirect_uri = '/relative/uri/'
+    self.client.save()
+
+    with self.assertRaises(InvalidRequest):
+      response = self.oauth_client.make_authorization_request(
+          client_id=self.client.key,
+          scope_string=self.oauth_client.scope_string,
+          endpoint=self.dummy_endpoint_uri)
+
+    error_redirect = auth_code_generator.make_error_redirect()
+
+    # Show that the error redirect was directed to the default missing redirect
+    # URI, and not the registered, and invalid, URI.
+    parsed_redirect_uri = urlparse(error_redirect.get('location'))
+    redirect_location = urlunparse((
+      parsed_redirect_uri.scheme,
+      parsed_redirect_uri.netloc,
+      parsed_redirect_uri.path,
+      '',
+      '',
+      ''))
+
+    self.assertEqual(redirect_location, self.missing_redirect_uri)
+    self.assertNotEqual(redirect_location, self.client.redirect_uri)
 
 
 class TestMakeAuthorizationEndpointHelper(DJOAuth2TestCase):
