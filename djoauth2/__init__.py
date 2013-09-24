acefee0b637c4a8786ff02c969a6d004fa56a276
Peter Downs <peterldowns@gmail.com>
2013-09-23 17:46:37 -0700

    Bump version.

--- djoauth2/__init__.py
+++ djoauth2/__init__.py
@@ -1,2 +1,2 @@
 # coding: utf-8
-__VERSION__ = '0.2.0'
+__VERSION__ = '0.2.1'
--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='djoauth2',
-    version='0.2.0',
+    version='0.2.1',
     packages=['djoauth2', 'djoauth2.tests'],
     include_package_data=True,
     license='MIT License',
     description='OAuth 2.0 server implementation.',
     long_description=README,
     url='https://github.com/peterldowns/djoauth2/',
-    download_url='https://github.com/peterldowns/djoauth2/tarball/0.2.0',
+    download_url='https://github.com/peterldowns/djoauth2/tarball/0.2.1',
     keywords=['oauth', 'oauth2', 'django'],
     install_requires=REQUIREMENTS,
     author='Peter Downs',
